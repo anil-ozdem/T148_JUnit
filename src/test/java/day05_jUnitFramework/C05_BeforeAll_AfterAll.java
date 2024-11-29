415f9f4fe61fb58a971db65ba2ac5506520e5d69
anilozdem <anil.ozdem2134@gmail.com>
2024-11-29 13:16:13 +0300

    commit

---
+++ src/test/java/day05_jUnitFramework/C05_BeforeAll_AfterAll.java
@@ -0,0 +1,13 @@
+package day05_jUnitFramework;
+
+public class C05_BeforeAll_AfterAll {
+
+    // 3 farkli test method'u olusturarak asagidaki testleri gerceklestirin
+    // 1- Test otomasyonu anasayfaya gidin
+    //    Url'in test otomasyonu icerdigini test edin
+    // 2- phone icin arama yapin
+    //    ve arama sonucunda urun bulunabildigini test edin
+    // 3- ilk urunu tiklayin
+    //    ve acilan sayfadaki urun isminde case sensitive olamdan "phone" bulundugunu test edin
+
+}
