5e4924a84ee3b5febd284eb114b5e4c33b6f9685
anilozdem <anil.ozdem2134@gmail.com>
2024-11-29 13:45:25 +0300

    day05 sonu

--- src/test/java/day05_jUnitFramework/C05_BeforeAll_AfterAll.java
+++ src/test/java/day05_jUnitFramework/C05_BeforeAll_AfterAll.java
@@ -1,5 +1,17 @@
 package day05_jUnitFramework;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+import java.util.List;
+
 public class C05_BeforeAll_AfterAll {
 
     // 3 farkli test method'u olusturarak asagidaki testleri gerceklestirin
@@ -9,5 +21,84 @@ public class C05_BeforeAll_AfterAll {
     //    ve arama sonucunda urun bulunabildigini test edin
     // 3- ilk urunu tiklayin
     //    ve acilan sayfadaki urun isminde case sensitive olamdan "phone" bulundugunu test edin
+
+    /*
+        Bu gorev icin
+        her test method'undan sonra driver'i kapatmak (@AfterEach) mantikli olmaz
+
+        bunun yerine
+        class calismaya basladiginda hic bir method calismadan once driver'i olusturmak
+        ve tum @Test method'lar calisip bittikten sonra
+        calisacak @Test method'u kalmadiginda
+        driver'i kapatmak daha mantikli olur
+
+        Bu tur birbirinin sonunca bagli test method'lari oldugunda
+        JUnit ile method'lari tek tek run edebiliriz
+        ancak method'un yapmasi gereken islevi yapmasi mumkun olmayabilir
+
+        @BeforeAll ve @AfterAll notasyonu kullanan method'larin
+        mutlaka static olmasi gerekir
+             */
+    static WebDriver driver;
+    @BeforeAll
+    static void setup(){
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+    }
+
+    @AfterAll
+    static void teardown(){
+        driver.quit();
+    }
+
+    @Test
+    public void test01(){ // anasayfaTesti
+        // 1- Test otomasyonu anasayfaya gidin
+        driver.get("https://www.testotomasyonu.com");
+        //    Url'in testotomasyonu icerdigini test edin
+
+        String expectedUrlIcerik = "testotomasyonu";
+        String actualUrl = driver.getCurrentUrl();
+
+        if (actualUrl.contains(expectedUrlIcerik)){
+            System.out.println("Url testi PASSED");
+        } else System.out.println("Url testi FAILED");
+
+    }
+
+    @Test
+    public void test02(){ // urunAramaTesti
+        // 2- phone icin arama yapin
+        WebElement aramaKutusu = driver.findElement(By.id("global-search"));
+        aramaKutusu.sendKeys("phone"+ Keys.ENTER);
+        //    ve arama sonucunda urun bulunabildigini test edin
+        List<WebElement> bulunanUrunElementleriList = driver.findElements(By.xpath("//*[@*='prod-img']"));
+
+        int actualBulunanUrunSayisi = bulunanUrunElementleriList.size();
+
+        if (actualBulunanUrunSayisi>0){
+            System.out.println("urun bulma testi PASSED");
+        } else System.out.println("urun bulma testi FAILED");
+    }
+
+    @Test
+    public void test03(){ // ilkUrunIsimTesti
+        // 3- ilk urunu tiklayin
+        driver.findElement(By.xpath("(//*[@*='prod-img'])[1]"))
+                .click();
+
+        //    ve acilan sayfadaki urun isminde case sensitive olamdan "phone" bulundugunu test edin
+
+        WebElement ilkUrunIsimElementi = driver.findElement(By.xpath("//*[@class=' heading-sm mb-4']"));
+
+        String expectedIsimIcerik = "phone";
+        String actualUrunIsmi = ilkUrunIsimElementi.getText().toLowerCase();
+
+        if (actualUrunIsmi.contains(expectedIsimIcerik)){
+            System.out.println("Ilk urun isim testi PASSED");
+        } else System.out.println("Ilk urun isim testi FAILED");
+    }
 
 }
